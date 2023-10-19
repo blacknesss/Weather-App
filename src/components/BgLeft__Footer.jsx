fda820b4471f5c694d03706290049cfc2d370d68
hikkosha <hikky93@mail.ru>
2023-10-19 21:46:53 +0300

    new file:   src/assets/Line 17.svg 	modified:   src/components/BgLeft__Footer.jsx 	modified:   src/index.css

--- src/components/BgLeft__Footer.jsx
+++ src/components/BgLeft__Footer.jsx
@@ -1,28 +1,60 @@
-import group from '../assets/Group 650.svg'
+import group from '../assets/Group 650.svg';
+import line17 from '../assets/Line 17.svg';
+
 
 function BgLeft__Footer() {
+    let offset = 0;
+
+    const slider = () =>{
+        const containerF = document.querySelector('.little-foot');
+        offset = offset +=173;
+        if(offset > 519){
+            offset = 0
+        }
+        containerF.style.transform = `translateX(${-offset}px)`;
+    }
+
+
     return ( 
         <div className="Footer">
+            <div className='little-foot'>
             <div className="container-f">
-                <h1 className='up'>24</h1>
+                <h1 className='up'>24°C</h1>
                 <img src={group} alt="t" />
                 <h1 className='up'>Fri</h1>
             </div>
             <div className="container-f">
-                <h1 className='up'>24</h1>
+                <h1 className='up'>24°C</h1>
                 <img src={group} alt="t" />
-                <h1 className='up'>Fri</h1>
+                <h1 className='up'>Sat</h1>
             </div>
             <div className="container-f">
-                <h1 className='up'>24</h1>
+                <h1 className='up'>24°C</h1>
                 <img src={group} alt="t" />
-                <h1 className='up'>Fri</h1>
+                <h1 className='up'>Sun</h1>
             </div>
             <div className="container-f">
-                <h1 className='up'>24</h1>
+                <h1 className='up'>24°C</h1>
                 <img src={group} alt="t" />
-                <h1 className='up'>Fri</h1>
+                <h1 className='up'>Mon</h1>
             </div>
+            <div className="container-f">
+                <h1 className='up'>24°C</h1>
+                <img src={group} alt="t" />
+                <h1 className='up'>Tue</h1>
+            </div>
+            <div className="container-f">
+                <h1 className='up'>24°C</h1>
+                <img src={group} alt="t" />
+                <h1 className='up'>Wed</h1>
+            </div>
+            <div className="container-f">
+                <h1 className='up'>24°C</h1>
+                <img src={group} alt="t" />
+                <h1 className='up'>Thu</h1>
+            </div>
+            </div>
+            <div onClick={slider} className='line-next'><img src={line17} alt="err" /></div>
         </div>
      );
 }
